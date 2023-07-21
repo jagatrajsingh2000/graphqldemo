1fe84741304c5377f98cc93f40c2850cf269e2f7
jagatrajsingh2000 <jagatrajsingh2000@gmail.com>
2023-07-21 13:18:45 +0530

    updated:completed the demo project with user api, get all user and todo user

--- index.js
+++ index.js
@@ -1,32 +1,113 @@
-const express = require('express')
-const {ApolloServer} = require('@apollo/server')
-const { expressMiddleware } = require('@apollo/server/express4')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
 
-async function startServer(){
-    const app = express();
-    const server = new ApolloServer({
-        typeDefs: `
+const { USERS } = require("./user");
+const { TODOS } = require("./todo");
+
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+        type User {
+            id: ID!
+            name: String!
+            username: String!
+            email: String!
+            phone: String!
+            website: String!
+        }
+
         type Todo {
             id: ID!
             title: String!
             completed: Boolean
+            user: User
         }
+
         type Query {
             getTodos: [Todo]
+            getAllUsers: [User]
+            getUser(id: ID!): User
         }
 
-        `,
-        resolvers: {}
-    });
-    app.use(bodyParser.json());
-    app.use(cors());
-    
-    await server.start();
+    `,
+    resolvers: {
+      Todo: {
+        user: (todo) => USERS.find((e) => e.id === todo.id),
+      },
+      Query: {
+        getTodos: () => TODOS,
+        getAllUsers: () => USERS,
+        getUser: async (parent, { id }) => USERS.find((e) => e.id === id),
+      },
+    },
+  });
 
-    app.use("/graphql", expressMiddleware(server));
+  app.use(bodyParser.json());
+  app.use(cors());
 
-    app.listen(8000, () => console.log("server started at post 8000"))
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(8000, () => console.log("Serevr Started at PORT 8000"));
 }
-startServer()
+
+startServer();
+// const express = require('express')
+// const {ApolloServer} = require('@apollo/server')
+// const { expressMiddleware } = require('@apollo/server/express4')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const axios = require('axios')
+
+// async function startServer(){
+//     const app = express();
+//     const server = new ApolloServer({
+//         typeDefs: `
+//         type User {
+//             id: ID!
+//             name: String!
+//             username: String!
+//             email:String!
+//             phone: String!
+//             website: String!
+//         }
+//         type Todo {
+//             id: ID!
+//             title: String!
+//             completed: Boolean
+//             user: User
+//         }
+//         type Query {
+//             getTodos: [Todo]
+//             getAllUsers: [User]
+//             getUser(id: ID!): User
+//         }
+
+//         `,
+//         resolvers: {
+//             Todo: {
+//                 user: async(todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.id}`)).data
+//             },
+//             Query: {
+//                 getTodos: async() => (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+//                 getAllUsers: async () =>(await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+//                 getUser: async (parent, {id}) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data
+//             }
+//         }
+//     });
+//     app.use(bodyParser.json());
+//     app.use(cors());
+    
+//     await server.start();
+
+//     app.use("/graphql", expressMiddleware(server));
+
+//     app.listen(8000, () => console.log("server started at post 8000"))
+// }
+// startServer()
